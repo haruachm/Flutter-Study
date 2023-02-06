4492efd2140e3bca20ccf9697e0244fb3e83d0e1
haruachm <horang_e7@naver.com>
2023-02-06 10:20:16 +0900

    modify file structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,48 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_arrange/screen/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SizedBox(
-          height: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 75.0,
-                width: 75.0,
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    border: Border.all(
-                      width: 10,
-                      color: Colors.black,
-                    )),
-              ),
-              const SizedBox(width: 12.0),
-              Container(
-                height: 75.0,
-                width: 75.0,
-                color: Colors.green,
-              ),
-              const SizedBox(width: 12.0),
-              Container(
-                height: 75.0,
-                width: 75.0,
-                color: Colors.yellow,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(HomeScreen());
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 75.0,
+                width: 75.0,
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    border: Border.all(
+                      width: 10,
+                      color: Colors.black,
+                    )),
+              ),
+              const SizedBox(width: 12.0),
+              Container(
+                height: 75.0,
+                width: 75.0,
+                color: Colors.green,
+              ),
+              const SizedBox(width: 12.0),
+              Container(
+                height: 75.0,
+                width: 75.0,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
