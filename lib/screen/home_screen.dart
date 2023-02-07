6d23387daa8523fdb1d59e5d47f65de5e3d9e280
Haruachm <horang_e7@naver.com>
2023-02-07 13:51:35 +0900

    Learn MediaQuery.of(context).size

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,16 +6,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: Column(
-          children: [
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 50,
-            ),
-          ],
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.green,
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.purple,
+                width: 50,
+                height: 50,
+              ),
+            ],
+          ),
         ),
       ),
     );
