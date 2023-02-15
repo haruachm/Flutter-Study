9a302fb7a776d4fd3781d56880264ea87e319cd6
Haruachm <horang_e7@naver.com>
2023-02-15 17:03:20 +0900

    feat : Add Drawer Menu Bar

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,13 +8,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('앱바 만들기'),
+        title: Text('드로어 만들기'),
         centerTitle: true,
         elevation: 0.0,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.menu),
-        ),
         actions: [
           IconButton(
             onPressed: () {},
@@ -24,6 +21,72 @@ class HomeScreen extends StatelessWidget {
             icon: Icon(Icons.person),
           )
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/image/dog.jpg'),
+              ),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/image/yanado.jpg'),
+                ),
+              ],
+              accountName: Text('화니네'),
+              accountEmail: Text('abc12356@naver.com'),
+              onDetailsPressed: () {},
+              decoration: BoxDecoration(
+                color: Colors.purple[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              iconColor: Colors.purple,
+              focusColor: Colors.purple,
+              title: Text('홈'),
+              onTap: () {},
+              trailing: Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: Icon(Icons.shopping_cart_rounded),
+              iconColor: Colors.purple,
+              focusColor: Colors.purple,
+              title: Text('쇼핑'),
+              onTap: () {},
+              trailing: Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: Icon(Icons.mark_as_unread_sharp),
+              iconColor: Colors.purple,
+              focusColor: Colors.purple,
+              title: Text('편지함'),
+              onTap: () {},
+              trailing: Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: Icon(Icons.restore_from_trash),
+              iconColor: Colors.purple,
+              focusColor: Colors.purple,
+              title: Text('휴지통'),
+              onTap: () {},
+              trailing: Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              iconColor: Colors.purple,
+              focusColor: Colors.purple,
+              title: Text('설정'),
+              onTap: () {},
+              trailing: Icon(Icons.navigate_next),
+            ),
+          ],
+        ),
       ),
     );
   }
