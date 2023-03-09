b5cdd6e939f6736077051eda991a93166b9e409e
haruachm <horang_e7@naver.com>
2023-03-09 17:18:17 +0900

    Study : Flutter Button Widget

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -8,82 +8,27 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('드로어 만들기'),
-        centerTitle: true,
-        elevation: 0.0,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.person),
-          )
-        ],
+        title: Text('Button'),
       ),
-      drawer: Drawer(
-        child: ListView(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/image/dog.jpg'),
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/image/yanado.jpg'),
-                ),
-              ],
-              accountName: Text('화니네'),
-              accountEmail: Text('abc12356@naver.com'),
-              onDetailsPressed: () {},
-              decoration: BoxDecoration(
-                color: Colors.purple[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10.0),
-                  bottomRight: Radius.circular(10.0),
-                ),
-              ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('ElevatedButton'),
             ),
-            ListTile(
-              leading: Icon(Icons.home),
-              iconColor: Colors.purple,
-              focusColor: Colors.purple,
-              title: Text('홈'),
-              onTap: () {},
-              trailing: Icon(Icons.navigate_next),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('OutlinedButton'),
             ),
-            ListTile(
-              leading: Icon(Icons.shopping_cart_rounded),
-              iconColor: Colors.purple,
-              focusColor: Colors.purple,
-              title: Text('쇼핑'),
-              onTap: () {},
-              trailing: Icon(Icons.navigate_next),
-            ),
-            ListTile(
-              leading: Icon(Icons.mark_as_unread_sharp),
-              iconColor: Colors.purple,
-              focusColor: Colors.purple,
-              title: Text('편지함'),
-              onTap: () {},
-              trailing: Icon(Icons.navigate_next),
-            ),
-            ListTile(
-              leading: Icon(Icons.restore_from_trash),
-              iconColor: Colors.purple,
-              focusColor: Colors.purple,
-              title: Text('휴지통'),
-              onTap: () {},
-              trailing: Icon(Icons.navigate_next),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              iconColor: Colors.purple,
-              focusColor: Colors.purple,
-              title: Text('설정'),
-              onTap: () {},
-              trailing: Icon(Icons.navigate_next),
+            TextButton(
+              onPressed: () {},
+              child: Text('TextButton'),
             ),
           ],
         ),
