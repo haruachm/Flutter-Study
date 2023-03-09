a51e39fc7faa81511a54f52f5f4ab3dba0cb4817
haruachm <horang_e7@naver.com>
2023-03-09 20:43:29 +0900

    Study : ElevatedButton

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -20,15 +20,26 @@ class HomeScreen extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                // 글자 배경색 설정
+                backgroundColor: Colors.red,
+                // 글자 색상 및 애니메이션 색 설정
+                foregroundColor: Colors.yellow,
+                // 글자 그림자 설정
+                shadowColor: Colors.blue,
+                // 글자 3D 입체감 높이
+                elevation: 10.0,
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30.0,
+                ),
+                padding: EdgeInsets.all(50.0),
+                side: BorderSide(
+                  color: Colors.black,
+                  width: 5.0,
+                ),
+              ),
               child: Text('ElevatedButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('OutlinedButton'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('TextButton'),
             ),
           ],
         ),
