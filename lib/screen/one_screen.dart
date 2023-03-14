fc280792643380c9507a264d039571c53491c7ac
haruachm <horang_e7@naver.com>
2023-03-14 14:46:12 +0900

    Study: Move Screen

--- lib/screen/one_screen.dart
+++ lib/screen/one_screen.dart
@@ -2,24 +2,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_arrange/layout/main_layout.dart';
 
-import 'one_screen.dart';
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class OneScreen extends StatelessWidget {
+  const OneScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MainLayout(
-      title: 'Home Screen',
+      title: 'First Screen',
       children: [
         ElevatedButton(
           onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (BuildContext context) => OneScreen(),
-            ));
+            Navigator.of(context).pop();
           },
-          child: Text('첫번째 페이지 이동'),
-        ),
+          child: Text('뒤로가기'),
+        )
       ],
     );
   }
---
+++ lib/screen/two_screen.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class TwoScreen extends StatelessWidget {
+  const TwoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('One Screen'),
+      ),
+    );
+  }
+}
